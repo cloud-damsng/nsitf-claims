460badbcab855fd901e2861d78a3dd1f69679d30
cloud-damsng <cloud.damsng@gmail.com>
2022-05-07 09:00:04 +0100

    home component  modified

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -8,57 +8,57 @@ const Home =()=> {
     {
       "id": 1,
       "title": "Lagos Region",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 2,
       "title": "Ibadan Region",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 3,
       "title": "Abuja Region",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 4,
       "title": "Kaduna Region",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 5,
       "title": "Kano Region",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 6,
       "title": "Jos Region",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 7,
       "title": "Port Harcourt  Region",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 8,
       "title": "Asaba Region",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 9,
       "title": "Maiduguri Region",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 10,
       "title": "Bauch Region",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
     {
       "id": 11,
       "title": "Enugu Region",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+      "imageUrl": "https://ibb.co/ss4qTMh"
     },
   ];
     
